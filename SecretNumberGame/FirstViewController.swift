f37ec1def9ff071eb32baad9b2aadd62fb7f1b7d
Keith Mattix II <keithmattix2@gmail.com>
2017-09-23 03:07:57 -0500

    Initial Commit

---
+++ SecretNumberGame/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SecretNumberGame
+//
+//  Created by Keith Mattix on 9/23/17.
+//  Copyright © 2017 Keith Mattix. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SecretNumberGame/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SecretNumberGame
+//
+//  Created by Keith Mattix on 9/23/17.
+//  Copyright © 2017 Keith Mattix. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
